5e0c6779404892ec64c4bfffb5ad604d2117d69d
Rachel PR <rplourder@gmail.com>
2022-02-21 21:50:55 -0400

    Updated play.js to pass conn to setupInput

--- play.js
+++ play.js
@@ -1,10 +1,10 @@
 
 const net = require("net");
 const { connect } = require("./client")
-const {setupInput} = require("./input")
+const { setupInput } = require("./input")
 
 console.log("Connecting...")
-connect();
 
-setupInput();
+const conn = connect()
+setupInput(conn);
 
