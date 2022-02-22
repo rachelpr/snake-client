ceb197cf1a7f748de14afb2383621b2033775413
Rachel PR <rplourder@gmail.com>
2022-02-22 16:19:30 -0400

    Added constants.js to hold some constant values between input.js and client.js

---
+++ constants.js
@@ -0,0 +1,15 @@
+//A file to hold configuration constants
+const IP = "localhost"
+const PORT = 50541
+const MOVE_UP_KEY = "w"
+const MOVE_RIGHT_KEY = "d"
+const MOVE_DOWN_KEY = "s"
+const MOVE_LEFT_KEY = "a"
+const messages = {
+  "r": "Say: Race ya!",
+  "t": "Say: Yummmm",
+  "e": "Say: Weeeee",
+  "f": "Say: SssssS"
+};
+
+module.exports = { IP, PORT, MOVE_UP_KEY, MOVE_DOWN_KEY, MOVE_RIGHT_KEY, MOVE_LEFT_KEY, messages };
