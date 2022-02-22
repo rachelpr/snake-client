fa41f1028d6eb15ff660c05ed49b2cd22fb91a8c
Rachel PR <rplourder@gmail.com>
2022-02-21 21:16:02 -0400

    Refactored to have userInput and connect function imports

--- play.js
+++ play.js
@@ -1,23 +1,10 @@
+
 const net = require("net");
 const { connect } = require("./client")
+const {setupInput} = require("./input")
 
 console.log("Connecting...")
-connect()
+connect();
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+setupInput();
 
-const handleUserInput = function (key) {
-  if (key === "\u0003") {
-    process.exit();
-  }
-};
-
-
-setupInput()
